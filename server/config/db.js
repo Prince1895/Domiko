9ce54fc30d7cde867dc741ca107e04bccac0e961
Prince1895 <chauhanprince21153366@gmail.com>
2025-06-15 22:21:05 +0530

    update connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,21 +1,15 @@
 import mongoose from "mongoose";
 
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}domiko`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 20000, // important for Vercel cold start
-    });
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.log("MongoDB connection failed:", error.message);
-    process.exit(1); // Exit if DB fails
-  }
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}domiko`);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
-// Mongoose connection listeners (optional but helpful for logs)
-mongoose.connection.on('connected', () => console.log("Mongoose connected"));
+mongoose.connection.on('connected', () => console.log("database connected"));
 mongoose.connection.on('error', (err) => console.log("MongoDB connection error:", err));
 
 export default connectDB;
