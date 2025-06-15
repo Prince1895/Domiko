5efe2137dbc511e4a1b4c83faec27f73e987a037
Prince1895 <chauhanprince21153366@gmail.com>
2025-06-15 21:54:07 +0530

    update some more files

--- server/api.js
+++
@@ -1,5 +0,0 @@
-// api.js
-import serverlessExpress from '@vendia/serverless-express';
-import app from './index.js'; // import your Express app
-
-export const handler = serverlessExpress({ app });
